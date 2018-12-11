935dcef2a75156fafee2caca649e05fff303efbe
WangLei <wanglei0717@outlook.com>
2018-12-11 21:33:25 +0800

    Get integer interval from two number

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,7 +1,24 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  let collection = [];
+  let range = number_b - number_a;
+  let index = 0;
+  let number = number_a;
+  if (range === 0) {
+    collection[0] = number_a;
+  } else if (range > 0) {
+    for (index = 0; index < range + 1; index++) {
+      collection.push(number);
+      number++;
+    }
+  } else {
+    for (index = 0; index < (- range) + 1; index++) {
+      collection.push(number);
+      number--;      
+    }
+  }
+  return collection;
 }
 
 module.exports = get_integer_interval;
