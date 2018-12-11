8c75a35a242e573a981e521d3bee7475f576045f
WangLei <wanglei0717@outlook.com>
2018-12-11 22:40:20 +0800

    Find intersection of two collections

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  let intersection = [];
+  for (let i = 0; i < collection_b.length; i++) {
+    for (let j = 0; j < collection_a.length; j++) {
+      if (collection_b[i] === collection_a[j]) {
+        intersection.push(collection_b[i]);
+      }
+    }
+  }
+  return intersection;
 }
 
 module.exports = get_intersection;
