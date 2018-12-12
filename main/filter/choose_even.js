6352934e9d1ac3b15cd130bfa95907cf17d7d524
WangLei <wanglei0717@outlook.com>
2018-12-12 13:55:12 +0800

    Choose even from a collection

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_even(collection) {
+  let evenCollection = [];
+  for (let index = 0; index < collection.length; index++) {
+    if (isEven(collection[index])) {
+      evenCollection.push(collection[index]);
+    } 
+  }
+  return evenCollection;
+}
 
-  //在这里写入代码
+function isEven(number) {
+  return number % 2 === 0;
 }
 
 module.exports = choose_even;
