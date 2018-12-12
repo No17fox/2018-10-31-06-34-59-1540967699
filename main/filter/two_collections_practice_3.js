a4cbc16febfa9d84136517c6ef542cddb0dbfcbc
WangLei <wanglei0717@outlook.com>
2018-12-12 19:53:10 +0800

    Find elements that is divisible

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,22 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
+  let divisibleNumber = [];
+  for (let i = 0; i < collection_a.length; i++) {
+    if (isDivisible(collection_a[i], collection_b)) {
+      divisibleNumber.push(collection_a[i]);
+    }
+  }
+  return divisibleNumber;
+}
 
-  //在这里写入代码
+function isDivisible(number, collection) {
+  for (let i = 0; i < collection.length; i++) {
+    if (number % collection[i] === 0) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = choose_divisible_integer;
