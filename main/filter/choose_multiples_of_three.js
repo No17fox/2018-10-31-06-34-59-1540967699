2895d55e1e46b6392569f86e591419663e3c04d7
WangLei <wanglei0717@outlook.com>
2018-12-12 13:57:30 +0800

    Choose multiples of three from a collection

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
+  let evenCollection = [];
+  for (let index = 0; index < collection.length; index++) {
+    if (isMultipleOfThree(collection[index])) {
+      evenCollection.push(collection[index]);
+    } 
+  }
+  return evenCollection;
+}
 
-  //在这里写入代码
+function isMultipleOfThree(number) {
+  return number % 3 === 0;
 }
 
 module.exports = choose_multiples_of_three;
