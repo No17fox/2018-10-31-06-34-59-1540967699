444cf678f6ffba9e7f13eec0e74b2597bb0e6b9f
WangLei <wanglei0717@outlook.com>
2018-12-12 18:14:07 +0800

    Count elements repeat times in a collection

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //在这里写入代码
+  let elementType = {};
+  for (let i = 0; i < collection.length; i++) {
+    let differentTimes = 0;
+    for (const key in elementType) {
+      if (elementType.hasOwnProperty(key)) {
+        if (collection[i] === parseInt(key)) {
+          elementType[key] += 1;
+        } else {
+          differentTimes++;
+        }
+      }
+    }
+    if (differentTimes === Object.keys(elementType).length) {
+      elementType[collection[i]] = 1;
+    }
+  }
+  return elementType;
 }
 
 module.exports = grouping_count;
