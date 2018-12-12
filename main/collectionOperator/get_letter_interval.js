facddab16b4a6188e9486dfef77a93851d44f719
WangLei <wanglei0717@outlook.com>
2018-12-12 09:08:34 +0800

    Get letter interval from two number

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -1,7 +1,31 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  const range = number_b - number_a;
+  let index = 0;
+  let collection = [];
+  if (range === 0) {
+    pushAnChartInArray(collection, number_a);
+  } else if (range > 0) {
+    for (index; index < range + 1; index++) {
+      pushAnChartInArray(collection, number_a);
+      number_a++;
+    }
+  } else {
+    for (index; index < - range + 1; index++) {
+      pushAnChartInArray(collection, number_a);
+      number_a--;
+    }
+  }
+  return collection;
+}
+
+function pushAnChartInArray(array, number) {
+  return array.push(numberToString(number));
+}
+
+function numberToString(number) {
+  return String.fromCharCode(number + 96);
 }
 
 module.exports = get_letter_interval;
