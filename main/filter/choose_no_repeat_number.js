21ed78375da36e3cefb74cd67b40c7d33bf367c2
WangLei <wanglei0717@outlook.com>
2018-12-12 14:23:51 +0800

    Choose no repeat number in a collection

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
-  //在这里写入代码
+  let differentTimes = 0;
+  let elementType = [collection[0]];
+  for (let i = 0; i < collection.length; i++) {
+    for (let j = 0; j < elementType.length; j++) {
+      if (collection[i] != elementType[j]) {
+        differentTimes++;
+      }
+    }
+    if (differentTimes === elementType.length) {
+      elementType.push(collection[i]);
+    }
+    differentTimes = 0;
+  }
+  return elementType;
 }
 
 module.exports = choose_no_repeat_number;
