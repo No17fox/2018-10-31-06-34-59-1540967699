fb65554d7e5f6b3b744b4670dbca3f66046f8857
WangLei <wanglei0717@outlook.com>
2018-12-12 13:50:39 +0800

    Get union collection

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,7 +1,22 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  let collection = collection_a;
+  for (let i = 0; i < collection_b.length; i++) {
+    if (!isInCollection(collection_b[i], collection_a)) {
+      collection.push(collection_b[i]);
+    }
+  }
+  return collection;
+}
+
+function isInCollection(number, collection) {
+  for (let index = 0; index < collection.length; index++) {
+    if (number === collection[index]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = get_union;
