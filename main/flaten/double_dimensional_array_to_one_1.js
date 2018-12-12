5abec44539769e3fb4b22f5ff188799b62f30c36
WangLei <wanglei0717@outlook.com>
2018-12-12 20:20:44 +0800

    Convert double dimensional array to one dimensional array

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let oneDimArry = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] instanceof Array) {
+      for (let j = 0; j < collection[i].length; j++) {
+        oneDimArry.push(collection[i][j]);
+      }
+    } else {
+      oneDimArry.push(collection[i]);
+    }
+  }
+  return oneDimArry;
 }
 
 module.exports = double_to_one;
