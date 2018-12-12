1c02d76659f302d4a3b951d47731c075030a00f7
WangLei <wanglei0717@outlook.com>
2018-12-12 20:29:07 +0800

    Convert double dimensional array to one dimensional array and ignore repeating elements

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,32 @@
 'use strict';
 
 function double_to_one(collection) {
+  let oneDimArry = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] instanceof Array) {
+      for (let j = 0; j < collection[i].length; j++) {
+        pushUniqueElement(collection[i][j], oneDimArry);
+      }
+    } else {
+      pushUniqueElement(collection[i][j], oneDimArry);
+    }
+  }
+  return oneDimArry;
+}
 
-  //在这里写入代码
+function isInCollection(element, collection) {
+  for (let index = 0; index < collection.length; index++) {
+    if (element === collection[index]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function pushUniqueElement(element, collection) {
+  if(!isInCollection(element, collection)) {
+    collection.push(element);
+  }
 }
 
 module.exports = double_to_one;
