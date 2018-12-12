348feb519d81c1ccfbd8d3c67642a8c726888659
WangLei <wanglei0717@outlook.com>
2018-12-12 19:44:21 +0800

    Find different elements in two collections

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,8 +1,22 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
+  let uniqueElement = [];
+  for (let i = 0; i < collection_a.length; i++) {
+    if (!isInCollection(collection_a[i], collection_b)) {
+      uniqueElement.push(collection_a[i]);
+    }
+  }
+  return uniqueElement;
+}
 
-  //在这里写入代码
+function isInCollection(element, collection) {
+  for (let i = 0; i < collection.length; i++) {
+    if (element === collection[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = choose_no_common_elements;
