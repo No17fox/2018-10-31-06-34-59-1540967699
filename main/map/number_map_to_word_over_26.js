65256061dc48dcd532dfd3d73482845e5b794d39
WangLei <wanglei0717@outlook.com>
2018-12-12 22:22:02 +0800

    Map number to letter while the number is over 26

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,22 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(convertNumberToLetter);
 };
+
+function convertNumberToLetter(number) {
+  let multiple = number / 26;
+  let reminder = number % 26;
+  if (multiple <= 1) {
+    return numberToString(number);
+  } else if(number % 26 === 0) {
+    return numberToString(Math.floor(number) - 1) + numberToString(26);
+  } else {
+    return numberToString(Math.floor(multiple)) + numberToString(reminder);
+  }
+}
+
+function numberToString(number) {
+  return String.fromCharCode(number + 96);
+}
 
 module.exports = number_map_to_word_over_26;
