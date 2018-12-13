e43bc9004e186c0707cdd61b54744081162fb6bd
WangLei <wanglei0717@outlook.com>
2018-12-13 11:48:55 +0800

    Find the first even in a collection

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function find_first_even(collection) {
-  //在这里写入代码
+  let even = collection.reduce(isEven, []);
+  return collection[even.indexOf(1)];
+}
+
+function isEven(result, element, index) {
+  if (element % 2 === 0) {
+    result[index] = 1;
+  } else {
+    result[index] = 0;
+  }
+  return result;
 }
 
 module.exports = find_first_even;
