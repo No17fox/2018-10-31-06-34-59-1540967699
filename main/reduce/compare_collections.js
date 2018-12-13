0245f04e48842862aa45a124c9e5555ece7d1f01
WangLei <wanglei0717@outlook.com>
2018-12-13 09:12:46 +0800

    Compare two collections

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  return isSame(collection_a, collection_b).reduce(isAllTrue);
+}
+
+function isAllTrue(last, current) {
+  return last && current;
+}
+
+function isSame(collection_a, collection_b) {
+  return collection_a.map(function (element_a, i) {
+    return element_a === collection_b[i];
+  });
 }
 
 module.exports = compare_collections;
