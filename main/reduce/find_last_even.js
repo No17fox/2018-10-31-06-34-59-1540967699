ab76ca3f93bf1072deb1663cdc9eb5a708c39757
WangLei <wanglei0717@outlook.com>
2018-12-13 11:51:04 +0800

    Find the last even in a collection

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function find_last_even(collection) {
-  //在这里写入代码
+  let even = collection.reduce(isEven, []);
+  return collection[even.lastIndexOf(1)];
+}
+
+function isEven(result, element, index) {
+  if (element % 2 === 0) {
+    result[index] = 1;
+  } else {
+    result[index] = 0;
+  }
+  return result;
 }
 
 module.exports = find_last_even;
