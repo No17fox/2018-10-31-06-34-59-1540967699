8dc9c6b28b341d52cca45eeeaa3ff07697a4730f
WangLei <wanglei0717@outlook.com>
2018-12-13 09:46:22 +0800

    Compute median of elements in a collection

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -1,7 +1,26 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  let array = collection.split('->').map(Number).sort(compare);
+  return findMedian(array);
+}
+
+function findMedian(array) {
+  if (array.length % 2 === 0) {
+    return (array[array.length / 2 - 1] + array[array.length / 2]) / 2;
+  } else {
+    return array[(array.length - 1) / 2];
+  }
+}
+
+function compare(num_a, num_b) {
+  if (num_a < num_b) {
+    return -1;
+  } else if (num_a > num_b) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 module.exports = compute_chain_median;
