9bac950a609944cb159a0cdfec626da32a32a47c
WangLei <wanglei0717@outlook.com>
2018-12-14 21:25:07 +0800

    Sort even and odd separately

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,25 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
-
+  let even = collection.filter(isEven).sort(compare);
+  let odd = collection.filter(isOdd).sort(compare).reverse();
+  return even.concat(odd);
 };
+
+function compare(last, current) {
+  if (last < current) {
+    return -1;
+  } else if (last > current) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function isOdd(number) {
+  return number % 2 === 1;
+}
 module.exports = even_asc_odd_desc;
