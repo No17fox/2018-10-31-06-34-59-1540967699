1a406a5c636d651f71c8d8811dea0a77a579bc2b
WangLei <wanglei0717@outlook.com>
2018-12-14 20:09:42 +0800

    Convert the median of a collection to letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,33 @@
 'use strict';
 
 function median_to_letter(collection) {
+  return numberToLetter(median(collection));
+}
 
-  //在这里写入代码
+function median(collection) {
+  let middle = collection.length / 2;
+  if (collection.length % 2 === 0) {
+    return Math.ceil((collection[middle - 1] + collection[middle]) / 2);
+  } else {
+    return collection[Math.floor(middle)];
+  }
+}
+
+function numberToLetter(number) {
+  let multiple = number / 26;
+  let reminder = number % 26;
+  let firstCode = 0;
+  let lastCode = 0;
+  if (multiple <= 1) {
+    lastCode = number + 96;
+  } else if (reminder === 0) {
+    firstCode = multiple - 1 + 96;
+    lastCode = 26 + 96;
+  } else {
+    firstCode = Math.floor(multiple) + 96;
+    lastCode = reminder + 96;
+  }
+  return String.fromCharCode(firstCode) + String.fromCharCode(lastCode);
 }
 
 module.exports = median_to_letter;
