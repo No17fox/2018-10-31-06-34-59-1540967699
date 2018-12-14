1b84e1598df591bb202c8f2b444302ee22b9672b
WangLei <wanglei0717@outlook.com>
2018-12-14 17:15:11 +0800

    Count elements in a collection

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,25 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let elementType = [];
+  for (let i = 0; i < collection.length; i++) {
+    count(collection[i], elementType);
+  }
+  return elementType;
 }
+
+function count(element, collection) {
+  let differentTimes = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (element === collection[i].key) {
+      collection[i].count += 1;
+    } else {
+      differentTimes++;
+    }
+  }
+  if (differentTimes === collection.length) {
+    collection.push({key: element, count: 1});
+  }
+  differentTimes = 0;
+}
+
 
 module.exports = count_same_elements;
--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,5 +1,29 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let elementType = [];
+  for (let i = 0; i < collection.length; i++) {
+    let element = collection[i].split('-');
+    if (element[1]) {
+      elementType.push({key: element[0], count: Number(element[1])});
+    } else {
+      count(collection[i], elementType);
+    }  
+  }
+  return elementType;
+}
+
+function count(element, collection) {
+  let differentTimes = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (element === collection[i].key) {
+      collection[i].count += 1;
+    } else {
+      differentTimes++;
+    }
+  }
+  if (differentTimes === collection.length) {
+    collection.push({key: element, count: 1});
+  }
+  differentTimes = 0;
 }
 
 module.exports = count_same_elements;
