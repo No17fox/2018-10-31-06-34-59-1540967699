afb90ed717f96c86af696da3ae03d941c236426e
WangLei <wanglei0717@outlook.com>
2018-12-14 17:47:44 +0800

    Count element type in a collection and compare the collection with another object

--- main/section_3/practice_3/practice.js
+++ main/section_3/practice_3/practice.js
@@ -1,5 +1,15 @@
+var count_same_elements = require("../../../main/section_2/practice_1/practice.js");
+
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let elementType = count_same_elements(collection_a);
+  for (let i = 0; i < elementType.length; i++) {
+    for (let j = 0; j < object_b.value.length; j++) {
+      if (elementType[i].key === object_b.value[j]) {
+        elementType[i].count -= parseInt(elementType[i].count / 3);
+      }
+    }
+  }
+  return elementType;
 }
 
 module.exports = create_updated_collection;
