9a6bb4e490e4bc356f0a25dbce6989d42958d785
WangLei <wanglei0717@outlook.com>
2018-12-16 15:08:19 +0800

    Sort element according to complex rules

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,22 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  collection.sort(compare);
+  let multiple = Math.floor(collection.length / 3);
+  for (let i = 0; i < multiple; i++) {
+    let smallest = collection.splice(i * 3, 1);
+    collection.splice(i * 3 + 2, 0, smallest[0]);
+  }
+  return collection;
 }
+
+function compare(last, current) {
+  if (last < current) {
+    return -1;
+  } else if (last > current) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 module.exports = rank_by_two_large_one_small;
